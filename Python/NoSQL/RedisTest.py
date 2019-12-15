d34b6848b2dc02880d92ee7f21ca14a974f859c2
Robinpig <gujiangyunan@hotmail.com>
2019-12-16 07:55:46 +0800

    增加Redis与MongoDB的连接

---
+++ Python/NoSQL/RedisTest.py
@@ -0,0 +1,3 @@
+import redis
+#连接redis 之后使用client来使用redis
+client = redis.Redis(host='206.10.12.136', port=6379, password='1qaczv')
---
+++ Python/NoSQL/mongoDBTest.py
@@ -0,0 +1,3 @@
+from pymongo import MongoClient
+#MongoDB的连接
+client = MongoClient('mongodb://127.0.0.1:27017')
