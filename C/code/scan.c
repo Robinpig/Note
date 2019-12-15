bb197f3ac686dd5da3d539023e69f579ea003a4a
Robinpig <gujiangyunan@hotmail.com>
2019-12-15 15:42:22 +0800

    some location change

---
+++ C/code/scan.c
@@ -0,0 +1,11 @@
+//
+// Created by xx on 2019/12/15.
+//
+#include <stdio.h>
+int main (void) {
+    int no;
+    printf("Please input a int: ");
+    scanf("%d", &no);
+    printf("Your input number is: %d ", no);
+    return 0;
+}
