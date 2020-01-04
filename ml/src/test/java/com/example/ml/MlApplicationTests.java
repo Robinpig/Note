880be6900807736e9171b338b7043ce00a148c80
Robinpig <gujiangyunan@hotmail.com>
2020-01-04 12:48:34 +0800

    create java ml

---
+++ ml/src/test/java/com/example/ml/MlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
